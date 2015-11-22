18cb233b01278c14385061abfec622de3e2b124c
Michael Schuett <michaeljs1990@gmail.com>
2015-11-22 16:05:46 -0500

    Base autoloader
    Added a simple autoloader that you can hook into your bashrc or bash_profile. This lets you keep this folder in any directory and load in all custom and private sh files you may want to set.

---
+++ autoload.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# This file is loaded every time a new
+# terminal is opened things in this
+# file can't be dependent on a relative
+# path or you will see errors when you
+# open your terminal. Source this file
+# in your .bashrc or .bash_profile with
+#
+# `source dotfiles/autoload.sh`
+
+# Always get the directory that this script is set in
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Load all private bash files
+for PFILE in ${DOTFILES_DIR}/private/*
+do
+    FILE_NAME=$(basename ${PFILE})
+    if [ -f PFILE ]
+    then
+        echo "sourcing $FILE_NAME"
+        source $PFILE
+    fi
+done
+
+# Load all standard files
+for SFILE in ${DOTFILES_DIR}/src/*
+do
+    FILE_NAME=$(basename ${SFILE})
+    if [ -f SFILE  ]
+    then
+        echo "sourcing $(basename ${SFILE})"
+        source $SFILE
+    fi
+done
